983c7d45e89e5c89c7172e56860f22536473ad9b
Vineet Gulia <guliavineet8700@gmail.com>
2025-09-04 13:02:45 +0530

    Initial DPDP
    Installed dpdp vite + tailwind

---
+++ dpdp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='bg-amber-800'>DPDP</h1>
+  
+    </>
+  )
+}
+export default App
